98873449d97b8c4bd04d56326966ccf4b4fe8b07
Saadat Kosumbaeva <kosumbaevasaadat@gmail.com>
2022-01-15 13:10:45 +0600

    Created basic app structure.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CocktailsComponent } from './cocktails/cocktails.component';
+import { CocktailComponent } from './cocktails/cocktail/cocktail.component';
+import { CocktailDetailsComponent } from './cocktails/cocktail-details/cocktail-details.component';
+import { NewCocktailComponent } from './cocktails/new-cocktail/new-cocktail.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CocktailsComponent,
+    CocktailComponent,
+    CocktailDetailsComponent,
+    NewCocktailComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
